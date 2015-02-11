c2ff1ff2a1579480ca4c2110807a2f1b4a18b12d
vagrant <vagrant@precise32.(none)>
2015-02-11 21:05:11 +0000

    Mocha test passed

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,23 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a, b, c, d) {
+  return [a, b, c, d]
+};
 
-module.exports.firstAndLast = undefined;
+
+module.exports.firstAndLast = function(array) {   // pass in 1 argument (at least 3 element array)
+  array.splice(1, array.length - 2); //start at index 1 and remove length-2 elements
+  return array;
+};
+
+
+
+//module.exports.firstAndLast = function(array) {   // pass in 1 argument (at least 3 element array)
+//  return array.slice(1, array.length - 2); //start at index 1 and remove length-2 elements
+//};
+
+
+//array.length - 1
+
+//module.exports.firstAndLast = function(array) {   // pass in 1 argument (at least 3 element array)
+//  array.slice(1, -1); //start at index 1 and remove elements up until the last element
+//  return array;
+//};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,30 @@
-module.exports.addItem = undefined;
 
-module.exports.reverseSortedList = undefined;
+
+module.exports.addItem = function(itemYouWantToAdd, array){
+  var checkList = array.indexOf(itemYouWantToAdd);  //check index of itemYouWantToAdd and set it to a variable
+  if(checkList === -1){   //if variable equals -1 then add element to end of array
+  array.push(itemYouWantToAdd);
+  }
+};
+
+
+
+
+//module.exports.reverseSortedList = function(array){
+//  return array.reverse();
+//};
+
+
+
+module.exports.reverseSortedList = function(array){
+  array.reverse();
+  return array;
+};
+
+
+
+//var myArray = ['one', 'two', 'three'];
+//myArray.reverse(); 
+
+//console.log(myArray) // ['three', 'two', 'one']
+
