e088d1b5852165acfc7a735875dcce85a19b2d53
vagrant <vagrant@precise32.(none)>
2015-02-13 08:47:48 +0000

    All Mocha tests passed

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,13 +1,21 @@
 
 
-module.exports.addItem = function(itemYouWantToAdd, array){
+/*module.exports.addItem = function(itemYouWantToAdd, array){
   var checkList = array.indexOf(itemYouWantToAdd);  //check index of itemYouWantToAdd and set it to a variable
   if(checkList === -1){   //if variable equals -1 then add element to end of array
   array.push(itemYouWantToAdd);
   }
 };
+*/
 
 
+module.exports.addItem = function(itemYouWantToAdd, array){
+   //check index of itemYouWantToAdd and set it to a variable
+  if(array.indexOf(itemYouWantToAdd) == -1){   //if variable equals -1 then add element to end of array
+  array.push(itemYouWantToAdd);
+  }
+  return array;
+};
 
 
 //module.exports.reverseSortedList = function(array){
@@ -17,8 +25,7 @@ module.exports.addItem = function(itemYouWantToAdd, array){
 
 
 module.exports.reverseSortedList = function(array){
-  array.reverse();
-  return array;
+  return array.sort().reverse();
 };
 
 
