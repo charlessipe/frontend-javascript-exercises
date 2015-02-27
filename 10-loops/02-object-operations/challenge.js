fb6638b0561b2d59100c32b55da22f206be4696e
vagrant <vagrant@precise32.(none)>
2015-02-27 03:24:17 +0000

    Loop exercises passed

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -14,13 +14,36 @@ module.exports.extend = function(dest, src) {  // take two dictionaries as argum
   return dest;  // return a modified dest
 };
 
+
 module.exports.hasElems = function(anObject, anArray) {  //take two arguments. The first is an object and the second is an array of elements
 
-  return true;
+  if(anArray.length == 0) {
+      return true;
+    }
 
+  var rtnValue = true;
+
+  for(var index = 0; index < anArray.length; index++) { //loop through anArray
   
-  //return true when all of the elements in the array are keys in the object, false otherwise
+    if(anObject[anArray[index]] == null) {  //is each array element a key in the object?
+      rtnValue = false;
+      break;
+    }
+  }
+  return rtnValue;
 };
+
+/*
+    ✓ should be defined 
+    1) returns true for an empty array
+    ✓ returns true if it has all the keys 
+    2) returns false if one or more of the keys isn't in the hash
+
+Complete the hasElems function. This function should:
+
+take two arguments. The first is an object and the second is an array of elements
+return true when all of the elements in the array are keys in the object, false otherwise
+*/
 
 
 
